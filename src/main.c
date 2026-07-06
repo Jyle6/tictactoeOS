75c165431c0c627a29f8f20d475ed6c758111ca0
Jyle <drene7450@gmail.com>
2026-07-07 04:18:09 +0800

    add background

--- src/main.c
+++ src/main.c
@@ -1,11 +1,35 @@
 #include <efi/efi.h>
 #include <efi/efidef.h>
 #include <efi/efierr.h>
+#include <efi/efiprot.h>
+#include <efi/x86_64/efibind.h>
+
+void* memcpy(void* dest, const void* src, UINTN n) {
+    char* d = (char*)dest;
+    const char* s = (const char*)src;
+    for (UINTN i = 0; i < n; i++) {
+        d[i] = s[i];
+    }
+    return dest;
+}
 
 EFI_STATUS efi_main(EFI_HANDLE imagehandle, EFI_SYSTEM_TABLE *systemtable) {
-	systemtable->ConOut->OutputString(systemtable->ConOut, L"Ready to play tic-tac-toe?");
+	EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE gopmode;
+	systemtable->BootServices->LocateProtocol(&(EFI_GUID)EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID, NULL, (VOID*)&gop);
+	gopmode = *gop->Mode;
+	UINT32* vram;
+	UINT32* fb = (UINT32*)gopmode.FrameBufferBase;
+	UINTN fb_size = gopmode.FrameBufferSize;
+	UINTN memsize = (fb_size + 4095) / 4096;
+	systemtable->BootServices->AllocatePages(AllocateAnyPages, EfiLoaderData, memsize, (EFI_PHYSICAL_ADDRESS*)&vram);
+	for (UINTN i = 0; i < (fb_size / sizeof(UINT32)); i++) {
+        vram[i] = 0x002C3E50;
+    }
+	__builtin_memcpy(fb, vram, fb_size);
 	while (TRUE) {
-		__asm__ volatile("hlt");
+		__asm__("hlt");
 	}
+	systemtable->BootServices->FreePages((EFI_PHYSICAL_ADDRESS)vram, memsize);
 	return EFI_SUCCESS;
 }
